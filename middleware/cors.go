24e7ddb9f53ba035378bad16cd1955bdcddcdba0
SUXUING-star <jieyalinjiu@gmail.com>
2025-02-18 15:40:23 +0800

    up

--- middleware/cors.go
+++ middleware/cors.go
@@ -8,13 +8,14 @@ import (
 )
 
 func Cors() gin.HandlerFunc {
-	return cors.New(cors.Config{
+	config := cors.Config{
 		AllowOrigins: []string{
 			"http://localhost:5173",
 			"https://www.xingyunbi.site",
 			"https://xingyunbi.vercel.app",
 			"https://xingyunbi.site",
 			"https://xingyunbi-be.onrender.com",
+			"https://reactgo-backend-production.up.railway.app",
 		},
 		AllowMethods: []string{
 			"GET",
@@ -31,9 +32,18 @@ func Cors() gin.HandlerFunc {
 			"Content-Type",
 			"Authorization",
 			"Cache-Control",
+			"Accept",
+			"X-Requested-With",
 		},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
-	})
+	}
+
+	// 在开发模式下允许所有来源
+	if gin.Mode() != gin.ReleaseMode {
+		config.AllowAllOrigins = true
+	}
+
+	return cors.New(config)
 }
